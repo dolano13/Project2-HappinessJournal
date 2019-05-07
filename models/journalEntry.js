650d6a729cca78c1cc6a473f179169dd50de3475
Daniela Olano <danolan@Danielas-MacBook-Pro-2.local>
2019-05-07 16:57:00 -0400

    delete function

--- models/journalEntry.js
+++ models/journalEntry.js
@@ -3,8 +3,8 @@ const Schema   = mongoose.Schema;
 
 const thoughtsSchema = new Schema({
   author: {type: Schema.Types.ObjectId, ref:"User"},
-  date: { type: Date,default: Date.now} ,
-  Question:{type: String},
+  date: {type: Date,default: Date.now} ,
+  question:{type: String},
   song: String,
   mood:{ type: Number, min: 0, max: 5},
   pic: String,
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,16 @@ const dashboard = require('../models/stuff')
 const User = require("../models/user");
 const uploadCloud = require ("./cloudinary");
 
+
+var months = [
+  "January", "February", "March", "April",
+   "May", "June", "July", "August", "September",
+    "October", "November", "December"
+]
+// var d = post.date.getDate();
+// var m = monthNames[post.date.getMonth()];
+// var y = post.date.getFullYear();
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
@@ -22,18 +32,36 @@ router.get('/today', (req, res, next) => {
 
  router.post('/allPosts', uploadCloud.single('pic'), (req, res, next) => {
    let pic = req.file.url;
-   let Question = req.body.Question;
+   let question = req.body.question;
    let mood = req.body.bood;
    let song= req.body.SOTD;
    let author = req.user._id
 
-   journal.create({pic, Question, mood, song, author}).then(resFromDB=>{
+   journal.create({pic, question, mood, song, author}).then(resFromDB=>{
      res.redirect('/dashboard')
    })
+
+  //  router.get('/dashboard', isLoggedIn, (req, res, next) => {
+  //   User.findById(req.user._id).populate('favorites')
+  //   .then(userInfo => {
+  //     journal.find({author:req.user._id}).then(journalLog=> {
+  //       console.log("the user info when at the dashboard ---------- ", userInfo);
+  //       data = { thoughts:journalLog}
+  //       console.log("the info in the data object ========= ", data)
+  //       res.redirect('/allPosts', data)
+  //     })
+  //   })
+  //   .catch(err => {
+  //     next(err);
+  //   }) 
+  //  });
 
     console.log(req.body,req.file, '&^%&^%*&&%^&^%')
  })
 
+ router.get('/allPosts', (req, res, next) => {
+  res.render('thoughts/allPosts');
+});
  router.get('/chillout', (req, res, next) => {
   takeABreather.find().then(stuff =>{
     console.log(stuff)
@@ -69,11 +97,15 @@ router.get('/today', (req, res, next) => {
     .then(userInfo => {
       journal.find({author:req.user._id}).then(journalLog=> {
         console.log("the user info when at the dashboard ---------- ", userInfo);
+        var theDay = journalLog[0].date.getDate()
+        var theMonth = months[journalLog[0].date.getMonth()];
+        var theYear = journalLog[0].date.getFullYear();
         data = {
           userFaves: userInfo.favorites,
-          thoughts:journalLog
+          thoughts:journalLog,
+          theDate: `${theMonth} ${theDay}, ${theYear}`
         }
-        console.log("the info in the data object ========= ", data)
+        // console.log("the info in the data object ========= ", data, "================ ", theMonth, theDay, theYear)
         res.render('thoughts/mypage', data)
       })
     })
@@ -81,6 +113,22 @@ router.get('/today', (req, res, next) => {
       next(err);
     }) 
    });
+ 
+   router.post("/dashboard/delete/:id", isLoggedIn, (req, res, next) => {
+    console.log("Deleted a fave");
+    // dashboard.findOneAndDelete({ _id: req.params.id }).then(userFaves => {
+      req.user.favorites.pull(req.params.id)
+      req.user.save()
+      .then(updatedUser => {
+        console.log("the new user info ============== ", updatedUser);
+        res.redirect("/dashboard");
+
+      })
+      .catch(err => {
+        next(err)
+      })
+    });
+  // });
 
 
    function isLoggedIn(req, res, next){
